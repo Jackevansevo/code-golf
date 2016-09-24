0b3d8f047b0bf43e9749492f9d490144586f2e24
Jack Evans <jack@evans.gb.net>
2016-09-24 20:29:03 +0100

    Used destructuring instead of first/last

--- src/code_golf/problem8.clj
+++ src/code_golf/problem8.clj
@@ -6,11 +6,11 @@
 ;; integers (> 0), output the substring chain. You may assume that the sum of
 ;; all integers in the array does not exceed the length of the string.
 
-(defn word-chain [word slices]
+(defn word-chain [[word slices]]
   (map (fn [x y] (take x (drop y word))) slices (map - (reductions + slices) slices)))
 
 (defn -main [& args]
-  (map (fn [n] (word-chain (first n) (last n))) args))
+  (map (fn [n] (word-chain n)) args))
 
 (-main
   '("abcdefghijk" [2 1 3])
